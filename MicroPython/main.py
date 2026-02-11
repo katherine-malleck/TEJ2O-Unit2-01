e25ca407572a38106b0ce1a6e988dbac1ece53af
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-11 18:15:34 +0000

    name and comment

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,9 @@
 """
 Created by: Mr. Coxall
-Created on: Sep 2020
+Created on: Feb 2026
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Katherine!")
