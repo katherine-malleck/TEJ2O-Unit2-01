c38ec0fe9fbac5b3ddf0b93c12be0728b39264fd
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-11 12:41:38 -0500

    I changed it to hello Katherine, and edited the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Feb 2026
+ * This program will display Hello, Katherine
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Katherine!')
